e69d2b1cee8d7f5262b21116e767b8397b319884
Volodymyr Havryliuk <vova2341591@gmail.com>
2022-10-03 21:40:49 +0300

    made header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <header className='app__header'>
+      <h1 className='app__title'>Pokedex</h1>
+    </header>
+  );
+};
+
+export default App;
